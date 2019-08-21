5d6a38c3c2d7d9e2f5a0c57a47bcfaf4d9c8b0e6
GlinZachariah <glincool.zac@gmail.com>
2019-08-22 00:22:00 +0530

    added guest comp and service - browseTraining

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SigninComponent } from './signin/signin.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { MentorSignupComponent } from './mentor-signup/mentor-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    SigninComponent,
+    UserSignupComponent,
+    MentorSignupComponent
   ],
   imports: [
     BrowserModule,
