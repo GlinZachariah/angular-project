6882007b3b289ed4b7249f3b512c17b4917339ac
GlinZachariah <glincool.zac@gmail.com>
2019-08-22 07:44:06 +0530

    added login and signup route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { MentorSignupComponent } from './mentor-signup/mentor-signup.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: SigninComponent
+  },
+  {
+    path:'signin',
+    component:SigninComponent
+  },
+  {
+    path:'signup/mentor',
+    component:MentorSignupComponent
+  },
+  {
+    path:'signup/user',
+    component:UserSignupComponent
   }
 ];
 
