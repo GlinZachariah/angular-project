a3c4916bd0b3b1bb0bfac6c43b8e58ac42cd3834
GlinZachariah <glincool.zac@gmail.com>
2019-08-24 08:02:58 +0530

    checkpoint 3

--- src/app/data/user.model.ts
+++ src/app/data/user.model.ts
@@ -21,3 +21,30 @@ export class signUpUserForm {
 }
 
 
+export interface CourseDetails{
+  courseid:string;
+  coursename:string;
+  trainername:string;
+  technology:string;
+  charges:number;
+}
+
+export interface TrainingProgress{
+  coursedetail : CourseDetails
+  username:string;
+  progress:number;
+  rating:number;
+  status:string;
+  paymentstatus:string;
+  timeslot:string;
+  startdate:string;   //TODOD datatype of start date
+}
+
+export interface TrainingCompleted{
+  coursedetail:CourseDetails;
+  timeslot:string;
+  startdate: string;  //TODOD datatype of start date
+}
+
+
+
