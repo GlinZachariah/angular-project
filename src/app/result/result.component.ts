c5e70dc8db0aad12f688f5a0bfee1f3cf814cab0
glinzachariah <glinzac@gmail.com>
2019-08-30 04:43:09 +0530

    completed search result

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { SearchTrainingResult } from '../data.model';
+import { MainService } from '../main.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-result',
@@ -9,13 +11,22 @@ import { SearchTrainingResult } from '../data.model';
 export class ResultComponent implements OnInit {
   @Input() resultData: SearchTrainingResult[];
   results;
-  constructor() { }
+  constructor(private mainService: MainService, private route: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  ngOnChanges(changes: SimpleChanges){
+  ngOnChanges(changes: SimpleChanges) {
     this.results = changes['resultData'].currentValue;
   }
 
+  proposeTraining(Idx, result: SearchTrainingResult) {
+    document.getElementById(Idx).click();
+    if (!this.mainService.isLoggedIn) {
+      console.log(document.getElementById(Idx));
+      this.route.navigate(['signin']);
+    } else {
+      // TODO send POST request to and subscribe to get the status
+      console.log('Training Proposed');
+    }
+  }
 }
